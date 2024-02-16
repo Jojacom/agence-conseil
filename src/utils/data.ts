5493ae307cd09844fe4eaf36ff04880769550230
Mamadou Diagne <genova@dofbi.com>
2024-02-16 17:53:35 +0000

    ♻️ refactor (content): home update

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -1,40 +1,46 @@
 const services = [
     {
-        title: "Social Media Mastery",
-        description: "Augmentez votre visibilité et engagez votre audience.",
+        title: "Gestion des médias sociaux",
+        description: "Nous collaborons avec nos clients pour développer et entretenir une présence solide et captivante sur les réseaux sociaux, en diffusant un contenu de haute qualité et en favorisant l'interaction avec leur audience.",
         icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
         </svg>`,
     },
     {
-        title: "Optimisation SEO Expert",
-        description: "Boostez votre classement et dominez les résultats de recherche.",
+        title: "Dominez les médias sociaux",
+        description: "Augmentez votre visibilité et engagez votre audience dès aujourd'hui !",
         icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
         </svg>`,
     },
     {
-        title: "Publicité Ciblée",
-        description: "Des campagnes sur mesure pour maximiser votre impact.",
+        title: "Expert en optimisation SEO",
+        description: "Augmentez votre classement et prenez la tête des résultats de recherche",
         icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
         </svg>`,
     },
     {
-        title: "Contenu Captivant",
-        description: "Narrations qui laissent une empreinte mémorable.",
+        title: "Boostez votre influence",
+        description: "Campagnes publicitaires sur mesure pour atteindre votre public cible avec précision et accroître votre impact !",
         icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
         </svg>`,
     },
     {
-        title: "Data-Driven Insights",
-        description: "Des analyses approfondies pour guider vos décisions.",
+        title: "Contenu captivant et mémorable",
+        description: "Créez des récits qui marquent les esprits et inspirent l'action.",
         icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
         </svg>`,
     },
     {
-        title: "Stratégie Digitale Sur Mesure",
-        description: "Conseils personnalisés pour votre succès en ligne.",
+        title: "Illuminez vos choix stratégiques",
+        description: "Des analyses de données précises et pertinentes pour éclairer chaque décision.",
         icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
         </svg>`,
     },
+    {
+        title: "Stratégie digitale personnalisée",
+        description: "Des conseils sur mesure pour garantir votre succès en ligne.",
+        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
+        </svg>`,
+    }
 ];
 
 export { services };
