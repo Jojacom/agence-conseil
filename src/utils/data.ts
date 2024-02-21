562043a3996d830e00fd2c637625c13c2c5b2b2b
Mamadou Diagne <genova@dofbi.com>
2024-02-21 19:09:14 +0000

    ♻️ refactor (Data): services

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -1,11 +1,5 @@
 const services = [
     {
-        title: "Gestion des médias sociaux",
-        description: "Nous collaborons avec nos clients pour développer et entretenir une présence solide et captivante sur les réseaux sociaux, en diffusant un contenu de haute qualité et en favorisant l'interaction avec leur audience.",
-        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
-        </svg>`,
-    },
-    {
         title: "Dominez les médias sociaux",
         description: "Augmentez votre visibilité et engagez votre audience dès aujourd'hui !",
         icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
